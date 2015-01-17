f056b5e1d2b248ae23f122f02191baad2cfd482a
tiger <tiger3670@163.com>
2015-01-17 14:05:49 +0800

    modify test

--- test.php
+++ test.php
@@ -1,2 +1,28 @@
 <?php
-    echo "g";
+    //callback function
+    function my_callback_function() {
+    	echo "this is my_callback_function! \r\n";   	
+    }
+
+    //callback method_exists(object, method_name)od
+    class MyClass {
+        static function myCallbackMethod() {
+        	echo "hello callback method!\n";       
+        }
+    }
+
+    //simple callback
+    call_user_func('my_callback_function');
+
+    //simple method
+    call_user_func(array('MyClass', 'myCallbackMethod'));
+
+    //object method call
+    $obj = new MyClass();
+    call_user_func(array($obj, 'myCallbackMethod'));
+
+    //call
+
+
+
+ 
